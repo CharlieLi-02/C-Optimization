1fc5627d11373a8bf05d5ff7e8a030b1676b544a
CharlieLi-02 <114370505+CharlieLi-02@users.noreply.github.com>
2022-10-15 15:26:17 -0700

    Update List.cpp

--- assort/List.cpp
+++ assort/List.cpp
@@ -37,7 +37,7 @@ List::List(List&& other){
 }
 
 List::~List(){ // dont chage
-    for (ListNode *current = head, *next; current; current = next) {
+    for (Node *current = head, *next; current; current = next) {
     next = current->next;
     free(current);
     }
