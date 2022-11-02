942a983a85adc86980c615699c522da7f928d9cf
Loup_Solitaire <114370505+CharlieLi-02@users.noreply.github.com>
2022-11-02 16:53:53 -0700

    Add files via upload

--- polish/AST.cpp
+++ polish/AST.cpp
@@ -64,50 +64,11 @@ AST* AST::parse(const std::string& expression) {
             }
             //test if token is double, if not, throw runtime_error
             else {
-                size_t i = 0;
-                     size_t count = 0;
-                     char arr[token.size()];
-                     bool valid = false;
-                         if(token.substr(0,1).compare("+") || token.substr(0,1).compare("-")){
-                                 ++i;
-                         }
-                         for(; i < token.size(); i++){
-                             if(!isdigit(token[i])){
-                                 ++count ;
-                                 arr[count] = token[i];
-                                 }
-                             }
-                         if (count == 0){
-                             valid = true;
-                         }
-                         if (count == 1){
-                             if(arr[1] == '.'){
-                                 valid = true;
-                             }
-                         }
-                         else if (count == 2){
-                             if(arr[1] == 'e' && arr[2] == '-'){
-                                 valid = true;
-                             }
-                         }
-                         else if (count == 3){
-                             if(arr[1] == '.' && arr[2] == 'e' && (arr[3] == '+' || arr[3] == '-')){
-                                 valid = true;
-                             }
-                         }
-                        if(!valid){
-                            if(!stack.empty()) {
-                                stack.~Stack();
-                            }
-                            throw std::runtime_error("Invalid token: " + token);
-                        }
                 try {
                         std::string::size_type index;
                         double value = std::stod(token, &index);
                         if(index != token.size()){
-                            if(!stack.empty()) {
                                 stack.~Stack();
-                            }
                             throw std::runtime_error("Invalid token: " + token);
                         }
                         Node* current = new Node(value);
@@ -115,22 +76,17 @@ AST* AST::parse(const std::string& expression) {
                         num_operand++;
                 }
                 catch(const std::invalid_argument& error) {
-                    if(!stack.empty()) {
                         stack.~Stack();
-                    }
                     throw std::runtime_error("Invalid token: " + token);
                 }
             }
         }
-    
             //convert stack into AST;
             if(num_operand == 0) {
                 throw std::runtime_error("No input.");
             }
-            
             if(num_operand > 1) {
                 throw std::runtime_error("Too many operands.");
             }
-    
     return stack.pop();
 }
