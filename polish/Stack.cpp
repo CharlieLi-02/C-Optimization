313c869133fe22cfc19d96b42412eec58da41dec
Loup_Solitaire <114370505+CharlieLi-02@users.noreply.github.com>
2022-11-03 13:10:56 -0700

    Update Stack.cpp

--- polish/Stack.cpp
+++ polish/Stack.cpp
@@ -12,7 +12,7 @@ Stack::Stack(int size)
 // Destructor to free memory allocated to the stack
 Stack::~Stack() {
     for(int i = 0; i <= top; i++) {
-        //std::cout <<  "stack destructor is called " << arr[i] << std::endl;
+        std::cout <<  "stack destructor is called " << arr[i] << std::endl;
         if(arr[i] != nullptr) {
             delete arr[i];
         }
