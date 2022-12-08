d8101e2daa06964495cb82ce153fdb8f37926a80
Loup_Solitaire <114370505+CharlieLi-02@users.noreply.github.com>
2022-12-08 01:02:25 -0800

    Update Helpers.cpp

--- omnibus/Helpers.cpp
+++ omnibus/Helpers.cpp
@@ -17,7 +17,7 @@ vector<string> split(string str, string pattern)
     std::vector<std::string> result;
     str += pattern;//扩展字符串以方便操作
     size_t size = str.size();
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
     {
         pos = str.find(pattern, i);
         if (pos < size)
